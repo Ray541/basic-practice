416c50201c0c8780739e0f9e6dcfbfb7b6dcf428
Pranav Rao <pranavrao541@gmail.com>
2025-03-08 20:52:42 +0530

    refactor: change code without changing functionality

--- joker/app.js
+++ joker/app.js
@@ -3,14 +3,15 @@ const joke = document.querySelector(".joke");
 const url =
   "https://v2.jokeapi.dev/joke/Programming,Dark?blacklistFlags=nsfw,religious,political,racist,explicit";
 
-const getJoke = () => {
-  fetch(url)
-    .then((raw) => raw.json())
-    .then((data) => {
-      if (data.joke === undefined) {
-        return (joke.innerHTML = "Click Again!!!");
-      } else {
-        return (joke.innerHTML = `${data.joke}`);
-      }
-    });
+const getJoke = async () => {
+  const raw = await fetch(url);
+  const data = await raw.json();
+
+  if (data.joke === undefined && data.setup === undefined && data.delivery) {
+    return (joke.innerHTML = "Click Again!!!");
+  } else if (data.setup && data.delivery) {
+    return (joke.innerHTML = `${data.setup + data.delivery}`);
+  } else {
+    return (joke.innerHTML = `${data.joke}`);
+  }
 };
